ccd6d7635ede3663308585b68d706cad76da5c19
Jeremy Jacque <jeremy.jacque@algolia.com>
2019-02-06 16:59:33 +0100

    Add an action that will add a label to the specified issue

---
+++ actions/label_issue.py
@@ -0,0 +1,13 @@
+from lib.base import BaseJiraAction
+
+__all__ = [
+    'AddLabelJiraIssue'
+]
+
+
+class AddLabelJiraIssue(BaseJiraAction):
+    def run(self, issue_key, label_text):
+        issue = self._client.issue(issue_key)
+        issue.add_field_value("labels",label_text)
+        result = issue.fields.labels 
+        return result
