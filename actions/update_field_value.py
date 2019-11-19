4665971a94d2000f75a9a76b2db2d288d822a7c8
ytjohn <john@yourtech.us>
2019-11-19 13:19:10 -0500

    Add update_field_value action

---
+++ actions/update_field_value.py
@@ -0,0 +1,14 @@
+from lib.base import BaseJiraAction
+
+__all__ = [
+    'UpdateFieldValue'
+]
+
+
+class AddFieldValue(BaseJiraAction):
+    def run(self, issue_key, field, value, notify):
+        
+        issue = self._client.issue(issue_key)
+        issue.update(fields={field: value}, notify=notify)
+        result = issue.fields.labels
+        return result
