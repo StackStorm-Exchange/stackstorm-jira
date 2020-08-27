5219d8b20f62a989e979ada81f35513c094845c3
dylan-bitovi <dylan@bitovi.com>
2020-08-27 12:26:41 -0400

    added line break

--- actions/lib/formatters.py
+++ actions/lib/formatters.py
@@ -4,7 +4,8 @@ __all__ = [
 ]
 
 
-def to_issue_dict(issue, include_comments=False, include_attachments=False, include_customfields=False, include_components=False, include_subtasks=False):
+def to_issue_dict(issue, include_comments=False, include_attachments=False, 
+                  include_customfields=False, include_components=False, include_subtasks=False):
     """
     :rtype: ``dict``
     """
