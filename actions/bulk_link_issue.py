6389d5441e64982d9afceeb7b5137a03e22f9265
karamveer28 <karamveerkaur28@gmail.com>
2023-10-16 10:03:03 -0400

    Formatted code

--- actions/bulk_link_issue.py
+++ actions/bulk_link_issue.py
@@ -2,18 +2,30 @@ import threading
 from threading import Semaphore
 from lib.base import BaseJiraAction
 
+
 class BulkLinkJiraIssueAction(BaseJiraAction):
-    def link_issues(self,semaphore, issue_key=None, target_issue=None, direction=None, link_type=None):
+    def link_issues(
+        self,
+        semaphore,
+        issue_key=None,
+        target_issue=None,
+        direction=None,
+        link_type=None,
+    ):
         with semaphore:
-            if direction == 'outward':
+            if direction == "outward":
                 outward_issue_key = issue_key
                 inward_issue_key = target_issue
-                response = self._client.create_issue_link(link_type, inward_issue_key,outward_issue_key)
+                response = self._client.create_issue_link(
+                    link_type, inward_issue_key, outward_issue_key
+                )
 
-            if direction == 'inward':
+            if direction == "inward":
                 inward_issue_key = issue_key
                 outward_issue_key = target_issue
-                response = self._client.create_issue_link(link_type, inward_issue_key,outward_issue_key)
+                response = self._client.create_issue_link(
+                    link_type, inward_issue_key, outward_issue_key
+                )
             response_output = {"issue": target_issue, "response": response}
         print(response_output)
 
@@ -21,9 +33,12 @@ class BulkLinkJiraIssueAction(BaseJiraAction):
         threads = list()
         semaphore = Semaphore(10)
         for issue_key in issue_key_list:
-            x = threading.Thread(target=self.link_issues, args=(semaphore, issue_key, target_issue, direction, link_type))
+            x = threading.Thread(
+                target=self.link_issues,
+                args=(semaphore, issue_key, target_issue, direction, link_type),
+            )
             threads.append(x)
             x.start()
 
         for thread in threads:
-                thread.join()
+            thread.join()
