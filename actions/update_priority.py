66030425348bab8e0acdfda6a056917b99bf5885
Stefano <minguzzistefano@gmail.com>
2023-10-18 12:00:54 +0100

    Update update_priority.py

--- actions/update_priority.py
+++ actions/update_priority.py
@@ -8,5 +8,5 @@ __all__ = [
 class UpdatePriority(BaseJiraAction):
     def run(self, issue_key, new_priority, notify):
         issue = self._client.issue(issue_key)
-        issue.update(fields={priority: new_priority}, notify=notify)
+        issue.update(fields={"priority": new_priority}, notify=notify)
         return to_issue_dict(issue)
