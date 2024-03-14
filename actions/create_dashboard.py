0f458680d9f92c3b40cfd799d3ce7e282fa0b7a9
Justin Palmer <jpavlav@gmail.com>
2024-03-14 15:03:37 -0500

    Add Dashboard Support: #72 - Create Dashboard
    Added `create_dashboard` action.

---
+++ actions/create_dashboard.py
@@ -0,0 +1,21 @@
+from lib.base import BaseJiraAction
+from lib.utils import remove_empty_attributes
+
+__all__ = ["CreateJiraDashboardAction"]
+
+
+class CreateJiraDashboardAction(BaseJiraAction):
+    def run(
+        self, name, description=None, edit_permissions=None, share_permissions=None
+    ):
+        data = remove_empty_attributes(
+            {
+                "name": name,
+                "description": description,
+                "edit_permissions": edit_permissions,
+                "share_permissions": share_permissions,
+            }
+        )
+
+        dashboard = self._client.create_dashboard(**data)
+        return dashboard.raw
