a044aba8751a1c2916069747de0502cb4ed7d0d7
Justin Palmer <jpavlav@gmail.com>
2024-03-14 15:47:28 -0500

    Add Dashboard Support: #72 Add Gadget Action
    * Added `add_gadget` action. Allows the user to add a gadget to an
    existing dashboard.

---
+++ actions/add_gadget.py
@@ -0,0 +1,31 @@
+from lib.base import BaseJiraAction
+from lib.utils import remove_empty_attributes
+
+__all__ = ["AddGadgetAction"]
+
+
+class AddGadgetAction(BaseJiraAction):
+    def run(
+        self,
+        dashboard_id,
+        color=None,
+        ignore_uri_and_module_key_validation=None,
+        module_key=None,
+        position=None,
+        title=None,
+        uri=None,
+    ):
+        data = remove_empty_attributes(
+            {
+                "dashboard_id": dashboard_id,
+                "color": color,
+                "ignore_uri_and_module_key_validation": ignore_uri_and_module_key_validation,
+                "module_key": module_key,
+                "position": position,
+                "title": title,
+                "uri": uri,
+            }
+        )
+
+        gadget = self._client.add_gadget_to_dashboard(**data)
+        return gadget.raw
