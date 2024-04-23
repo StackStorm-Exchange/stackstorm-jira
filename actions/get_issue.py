0785da713d3387c323c12412fc6935ddf10b6dbf
Curtis Wilson <curtis.wilson@wpengine.com>
2024-04-23 13:57:45 -0500

    Adding ability to sanitize output for jinja templating (#79)
    * Adding ability to sanitize output for jinja templating

--- actions/get_issue.py
+++ actions/get_issue.py
@@ -9,7 +9,7 @@ __all__ = [
 class GetJiraIssueAction(BaseJiraAction):
     def run(self, issue_key, include_comments=False, include_attachments=False,
             include_customfields=False, include_components=False, include_subtasks=False,
-            include_links=False):
+            include_links=False, sanitize_formatting=False):
         issue = self._client.issue(issue_key)
         result = to_issue_dict(issue=issue, include_comments=include_comments,
                                include_attachments=include_attachments,
@@ -17,4 +17,15 @@ class GetJiraIssueAction(BaseJiraAction):
                                include_components=include_components,
                                include_subtasks=include_subtasks,
                                include_links=include_links)
-        return result
+
+        def strip_braces(data):
+            if isinstance(data, dict):
+                return {k: strip_braces(v) for k, v in data.items()}
+            elif isinstance(data, list):
+                return [strip_braces(element) for element in data]
+            elif isinstance(data, str):
+                return data.replace("{{", "").replace("}}", "")
+            else:
+                return data
+
+        return strip_braces(result) if sanitize_formatting else result
