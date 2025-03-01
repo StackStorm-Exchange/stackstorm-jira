486b27b151b423a72c04117cab3ebaf5247bccb4
Justin Palmer <jpavlav@gmail.com>
2025-03-01 10:03:29 -0600

    Allow users to update other fields during transition. (#84)

--- actions/transition_issue.py
+++ actions/transition_issue.py
@@ -1,12 +1,9 @@
 from lib.base import BaseJiraAction
 
-__all__ = [
-    'TransitionJiraIssueAction'
-]
+__all__ = ["TransitionJiraIssueAction"]
 
 
 class TransitionJiraIssueAction(BaseJiraAction):
-
-    def run(self, issue_key, transition):
-        result = self._client.transition_issue(issue_key, transition)
+    def run(self, issue_key, transition, fields):
+        result = self._client.transition_issue(issue_key, transition, fields=fields)
         return result
